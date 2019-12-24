1506080bf826a8226141f00e7956e7a667fbfed0
Linna Wang <185595618@qq.com>
2019-12-23 22:30:31 -0800

    upload build

---
+++ build/precache-manifest.f499a1f207032d82f9990013c18ac2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049030044236bf062b98cf4dc5d10d6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c865ecc94ed062a22f02",
+    "url": "/static/css/main.5a16d294.chunk.css"
+  },
+  {
+    "revision": "ef4a3320cd4be08779f9",
+    "url": "/static/js/2.323f9f7c.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.323f9f7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c865ecc94ed062a22f02",
+    "url": "/static/js/main.3d7af233.chunk.js"
+  },
+  {
+    "revision": "ef0ffb6c889e70881c9c",
+    "url": "/static/js/runtime-main.7cbee7b2.js"
+  }
+]);
